aaf0428af1de121aca92ab0ec33360d985642b75
douglasroberto90 <douglas_roberto90@hotmail.com>
2024-06-04 20:37:21 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
-import BordaColorida from './components/BordaColorida/BordaColorida'
+import './App.css';
+import BordaColorida from './components/BordaColorida/BordaColorida.jsx';
+import ContainerImagem from './components/ContainerImagem/ContainerImagem.jsx';
 
 export default function App() {
-  
+
   return (
     <div classname="App">
       <BordaColorida>
-      <p>O que farei ao chegar em casa? (resumidamente)</p>
-      <ol>
-        <li>Chegar em casa</li>
-        <li>Reclamar do dia</li>
-        <li>Ir dormir</li>
-      </ol>
+        <p>O que farei ao chegar em casa? (resumidamente)</p>
+        <ol>
+          <li>Chegar em casa</li>
+          <li>Reclamar do dia</li>
+          <li>Ir dormir</li>
+        </ol>
       </BordaColorida>
       <ContainerImagem legenda="Opaloso">
         <img src='./assets/opala.jpg' alt=''></img>
